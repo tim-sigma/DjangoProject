5622d0b43502c7ce1217652ea0b8524d1e239b29
Tymur <timgenz18@gmail.com>
2025-06-04 20:00:51 +0300

    Реалізував CRUD через токен

---
+++ django_project/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from main_app.views import InputPhraseViewSet, ReferencePhraseViewSet, PromptViewSet, ResultViewSet
+
+router = DefaultRouter()
+router.register('inputphrases', InputPhraseViewSet)
+router.register('referencephrases', ReferencePhraseViewSet)
+router.register('prompts', PromptViewSet)
+router.register('results', ResultViewSet)
+
+urlpatterns = [
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+]
