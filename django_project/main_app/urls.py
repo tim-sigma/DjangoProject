bf0e2d1a83e3dc3c0ea8946f500cceba4e407216
Tymur <timgenz18@gmail.com>
2025-07-04 20:24:10 +0300

    Реалізував додавання, перегляд, редагування та видалення записів для всіх моделей

--- django_project/main_app/urls.py
+++ django_project/main_app/urls.py
@@ -1,24 +1,52 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth.views import LogoutView
-from rest_framework.routers import DefaultRouter
 
-from .views import (
-    UserLoginView,
-    InputPhraseListView,
-    ReferencePhraseListView,
-    PromptListView,
-    ResultListView,
-    register,
-    index
-)
+import main_app.views as views
+
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('register/', register, name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('', views.index, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
-    path('inputphrases/', InputPhraseListView.as_view(), name='inputphrase_list'),
-    path('referencephrases/', ReferencePhraseListView.as_view(), name='referencephrase_list'),
-    path('prompts/', PromptListView.as_view(), name='prompt_list'),
-    path('results/', ResultListView.as_view(), name='result_list'),
+]
+
+# CreateViews
+urlpatterns += [
+    path('inputphrases/add/', views.InputPhraseCreateView.as_view(), name='inputphrase_add'),
+    path('referencephrases/add/', views.ReferencePhraseCreateView.as_view(), name='referencephrase_add'),
+    path('prompts/add/', views.PromptCreateView.as_view(), name='prompt_add'),
+    path('results/add/', views.ResultCreateView.as_view(), name='result_add'),
+]
+
+# ListViews
+urlpatterns += [
+    path('inputphrases/', views.InputPhraseListView.as_view(), name='inputphrase_list'),
+    path('referencephrases/', views.ReferencePhraseListView.as_view(), name='referencephrase_list'),
+    path('prompts/', views.PromptListView.as_view(), name='prompt_list'),
+    path('results/', views.ResultListView.as_view(), name='result_list'),
+]
+
+# DetailViews
+urlpatterns += [
+    path('inputphrases/<int:pk>/', views.InputPhraseDetailView.as_view(), name='inputphrase_detail'),
+    path('referencephrases/<int:pk>/', views.ReferencePhraseDetailView.as_view(), name='referencephrase_detail'),
+    path('prompts/<int:pk>/', views.PromptDetailView.as_view(), name='prompt_detail'),
+    path('results/<int:pk>/', views.ResultDetailView.as_view(), name='result_detail'),
+]
+
+# UpdateViews
+urlpatterns += [
+    path('inputphrases/<int:pk>/edit/', views.InputPhraseUpdateView.as_view(), name='inputphrase_edit'),
+    path('referencephrases/<int:pk>/edit/', views.ReferencePhraseUpdateView.as_view(), name='referencephrase_edit'),
+    path('prompts/<int:pk>/edit/', views.PromptUpdateView.as_view(), name='prompt_edit'),
+    path('results/<int:pk>/edit/', views.ResultUpdateView.as_view(), name='result_edit'),
+]
+
+# DeleteViews
+urlpatterns += [
+    path('inputphrases/<int:pk>/delete/', views.InputPhraseDeleteView.as_view(), name='inputphrase_delete'),
+    path('referencephrases/<int:pk>/delete/', views.ReferencePhraseDeleteView.as_view(), name='referencephrase_delete'),
+    path('prompts/<int:pk>/delete/', views.PromptDeleteView.as_view(), name='prompt_delete'),
+    path('results/<int:pk>/delete/', views.ResultDeleteView.as_view(), name='result_delete'),
 ]
--- django_project/main_app/views.py
+++ django_project/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 
@@ -9,6 +9,36 @@ from .forms import UserRegistrationForm
 # Представлення логіну
 class UserLoginView(LoginView):
     template_name = 'login.html'
+
+
+# CreateViews
+class InputPhraseCreateView(CreateView):
+    model = InputPhrase
+    fields = ['text']
+    template_name = 'create_form.html'
+    success_url = '/inputphrases/'
+
+class ReferencePhraseCreateView(CreateView):
+    model = ReferencePhrase
+    fields = ['text']
+    template_name = 'create_form.html'
+    success_url = '/referencephrases/'
+
+class PromptCreateView(CreateView):
+    model = Prompt
+    fields = ['input_phrase', 'reference_phrases', 'text']
+    template_name = 'create_form.html'
+    success_url = '/prompts/'
+
+class ResultCreateView(CreateView):
+    model = Result
+    fields = [
+        'input_phrase', 'prompt', 'matched_reference',
+        'explanation', 'similarity_ml', 'similarity_cosine',
+        'similarity_semantic', 'similarity_contextual'
+    ]
+    template_name = 'create_form.html'
+    success_url = '/results/'
 
 
 # ListViews
@@ -31,6 +61,80 @@ class ResultListView(ListView):
     model = Result
     template_name = 'result_list.html'
     context_object_name = 'results'
+
+
+# DetailViews
+class InputPhraseDetailView(DetailView):
+    model = InputPhrase
+    template_name = 'inputphrase_detail.html'
+    context_object_name = 'inputphrase'
+
+class ReferencePhraseDetailView(DetailView):
+    model = ReferencePhrase
+    template_name = 'referencephrase_detail.html'
+    context_object_name = 'referencephrase'
+
+class PromptDetailView(DetailView):
+    model = Prompt
+    template_name = 'prompt_detail.html'
+    context_object_name = 'prompt'
+
+class ResultDetailView(DetailView):
+    model = Result
+    template_name = 'result_detail.html'
+    context_object_name = 'result'
+
+
+# UpdateViews
+class InputPhraseUpdateView(UpdateView):
+    model = InputPhrase
+    fields = ['text']
+    template_name = 'update_form.html'
+    success_url = '/inputphrases/'
+
+class ReferencePhraseUpdateView(UpdateView):
+    model = ReferencePhrase
+    fields = ['text']
+    template_name = 'update_form.html'
+    success_url = '/referencephrases/'
+
+class PromptUpdateView(UpdateView):
+    model = Prompt
+    fields = ['input_phrase', 'reference_phrases', 'text']
+    template_name = 'update_form.html'
+    success_url = '/prompts/'
+
+class ResultUpdateView(UpdateView):
+    model = Result
+    fields = [
+        'input_phrase', 'prompt', 'matched_reference',
+        'explanation', 'similarity_ml', 'similarity_cosine',
+        'similarity_semantic', 'similarity_contextual'
+    ]
+    template_name = 'update_form.html'
+    success_url = '/results/'
+
+
+# DeleteViews
+class InputPhraseDeleteView(DeleteView):
+    model = InputPhrase
+    template_name = 'confirm_delete.html'
+    success_url = '/inputphrases/'
+
+class ReferencePhraseDeleteView(DeleteView):
+    model = ReferencePhrase
+    template_name = 'confirm_delete.html'
+    success_url = '/referencephrases/'
+
+class PromptDeleteView(DeleteView):
+    model = Prompt
+    template_name = 'confirm_delete.html'
+    success_url = '/prompts/'
+
+class ResultDeleteView(DeleteView):
+    model = Result
+    template_name = 'confirm_delete.html'
+    success_url = '/results/'
 
 
 # Функції
